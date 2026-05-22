d22f9f59e6511800a0d97b6f6356587c8366f2c4
Tousif2007 <tousif.sjws.2021@gmail.com>
2026-05-22 16:44:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -25,26 +25,26 @@ def calculate_stats(transcript):
     return total_credits, average
 
 
-# Master Database of UBC Science Specialization Requirements & Quotas
+
 # Database of UBC Science specializations, their history, and what you need to get in
 # Each entry maps out both the entry barrier (cutoffs) and the full 4-year path
 majors_database = {
     "Data Science": {
         "cutoffs": {2025: 76.0},
-        # Need one course from each group listed below
+        # Need one course from each group listed below 
         "first_year_reqs": [["MATH 100", "MATH 102", "MATH 104", "MATH 180"], "DSCI 100", ["CPSC 103", "CPSC 110"]],
         "grad_requirements": {
-            # The core courses you'll tackle in second year and beyond
+            # The core courses you'll need to take in second year and beyond
             "Second Year Core": ["DSCI 200", "DSCI 220", "DSCI 221", ["MATH 200", "MATH 226"], ["MATH 221", "MATH 152"], ["STAT 201", "STAT 200"]],
             "Upper Year Core": ["CPSC 330", "CPSC 368", "DSCI 310", "DSCI 320", "DSCI 430", "STAT 301"]
         }
     },
     "Biochemistry": {
         "cutoffs": {2025: 76.7, 2024: 65.9, 2023: 76.4, 2022: 71.0, 2021: 71.4},
-        # You need these specific foundations to start the major
+        # You need these specific required courses to start the major
         "first_year_reqs": ["BIOL 112", ["CHEM 121", "CHEM 111"], ["MATH 100", "MATH 102", "MATH 104", "MATH 180"]],
         "grad_requirements": {
-            # The essential classes to get your degree in Biochem
+            # The required classes to get your degree in Biochem
             "Second Year Core": [["BIOC 202", "BIOL 201"], "BIOL 200", ["CHEM 203", "CHEM 233"], ["CHEM 213", "CHEM 235"], "CHEM 205"],
             "Upper Year Core": ["BIOC 301", "BIOC 302", "BIOC 303", "BIOC 402", "BIOC 403", "BIOL 335"]
         }
